618cdf8b5a16a54433843cf3b68366c6f5bc8b7e
neonfly69 <120149722+neonfly69@users.noreply.github.com>
2024-03-19 15:26:26 -0600

    script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("Mar 18, 2024 00:00:00").getTime();
+var countDownDate = new Date("Mar 19, 2024 00:00:00").getTime();
 
 // Update the countdown every second
 var x = setInterval(function() {
